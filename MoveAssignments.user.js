3f1af9fb71b6f686866375f302f4668fa5b795a8
Will Austin <139591892+waustin-MSUD@users.noreply.github.com>
2026-05-15 15:12:30 -0600

    Update MoveAssignments.user.js

--- MoveAssignments.user.js
+++ MoveAssignments.user.js
@@ -4,7 +4,7 @@
 // @namespace     CTLD
 // @description   Select items on the Assignments page, move them to another assignment group.
 // @author        CTLD
-// @updateurl
+// @updateurl     https://raw.githubusercontent.com/waustin-MSUD/userscriptsforcanvas/refs/heads/main/MoveAssignments.user.js
 // @icon          https://du11hjcvx0uqb.cloudfront.net/br/dist/images/favicon-e10d657a73.ico
 // @match         https://*.instructure.com/courses/*/assignments
 // @match         https://*.instructure.com/courses/*/assignments?*
