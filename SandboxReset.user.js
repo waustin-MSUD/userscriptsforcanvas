bf4afa5860e70b91dd34cbd7ace5933472be5c9a
Will Austin <139591892+waustin-MSUD@users.noreply.github.com>
2026-05-15 15:15:45 -0600

    Update SandboxReset.user.js

--- SandboxReset.user.js
+++ SandboxReset.user.js
@@ -4,7 +4,7 @@
 // @namespace     CTLD
 // @description   Bulk deletion for assignments, quizzes, discussions, announcements, modules, and rubrics in a Sandbox.
 // @author        CTLD
-// @updateurl
+// @updateurl     https://raw.githubusercontent.com/waustin-MSUD/userscriptsforcanvas/refs/heads/main/SandboxReset.user.js
 // @icon          https://du11hjcvx0uqb.cloudfront.net/br/dist/images/favicon-e10d657a73.ico
 // @match         https://*.instructure.com/courses/*/assignments
 // @match         https://*.instructure.com/courses/*/quizzes
