204f1648eafe2d0f3a6c315b163bc9e558bbcd4d
Will Austin <139591892+waustin-MSUD@users.noreply.github.com>
2026-05-15 15:05:26 -0600

    Update SnippetInserter.user.js

--- SnippetInserter.user.js
+++ SnippetInserter.user.js
@@ -4,7 +4,7 @@
 // @namespace     CTLD
 // @description   Adds menu for inserting HTML snippets from the RCE.
 // @author        CTLD
-// @updateurl
+// @updateurl     https://raw.githubusercontent.com/waustin-MSUD/userscriptsforcanvas/refs/heads/main/SnippetInserter.user.js
 // @icon          https://du11hjcvx0uqb.cloudfront.net/br/dist/images/favicon-e10d657a73.ico
 // @match         https://*.instructure.com/courses/*/pages/*/edit
 // @match         https://*.instructure.com/courses/*/assignments/*/edit
