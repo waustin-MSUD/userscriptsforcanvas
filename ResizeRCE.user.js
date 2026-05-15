386c65ba73b0a14457cc8927309a077c106a5e14
Will Austin <139591892+waustin-MSUD@users.noreply.github.com>
2026-05-15 15:15:20 -0600

    Update ResizeRCE.user.js

--- ResizeRCE.user.js
+++ ResizeRCE.user.js
@@ -4,7 +4,7 @@
 // @namespace     CTLD
 // @description   Adjusts editor window according to the size of the browser window.
 // @author        CTLD
-// @updateurl
+// @updateurl     https://raw.githubusercontent.com/waustin-MSUD/userscriptsforcanvas/refs/heads/main/ResizeRCE.user.js
 // @icon          https://du11hjcvx0uqb.cloudfront.net/br/dist/images/favicon-e10d657a73.ico
 // @match         https://*.instructure.com/courses/*/pages/*/edit
 // @match         https://*.instructure.com/courses/*/assignments/*/edit
