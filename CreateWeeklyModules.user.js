4c7694b3c63dbd05d75b77d5855e792bb962bb4f
Will Austin <139591892+waustin-MSUD@users.noreply.github.com>
2026-05-15 15:09:09 -0600

    Update CreateWeeklyModules.user.js

--- CreateWeeklyModules.user.js
+++ CreateWeeklyModules.user.js
@@ -4,7 +4,7 @@
 // @namespace     CTLD
 // @description   Creates a configurable number of weekly modules from a chosen start date.
 // @author        CTLD
-// @updateurl
+// @updateurl     https://raw.githubusercontent.com/waustin-MSUD/userscriptsforcanvas/refs/heads/main/CreateWeeklyModules.user.js
 // @icon          https://du11hjcvx0uqb.cloudfront.net/br/dist/images/favicon-e10d657a73.ico
 // @match         https://*.instructure.com/courses/*/modules
 // @grant         GM_addStyle
